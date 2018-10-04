dfec75cf67fcb84cc80eb26a71df5f9bfa65dc1c
thawzinaung <thawzinaung@aceplussolutions.com>
2018-10-04 13:48:04 +0630

    Add click me button and change text

--- app/src/main/java/hivelocity/co/jp/fastlanetest/MainActivity.kt
+++ app/src/main/java/hivelocity/co/jp/fastlanetest/MainActivity.kt
@@ -1,12 +1,17 @@
 package hivelocity.co.jp.fastlanetest
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btnClickMe.setOnClickListener {
+            Toast.makeText(this@MainActivity, getString(R.string.this_is_fastlane_test), Toast.LENGTH_LONG).show()
+        }
     }
 }
